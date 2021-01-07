dc6ed6f4cc9274b29aa94c7c15faa6f88644e39a
jared Long <jlong4223@gmail.com>
2021-01-07 16:49:56 -0600

    removes console logs

--- src/pages/SelectionPage/SelectionPage.js
+++ src/pages/SelectionPage/SelectionPage.js
@@ -1,13 +1,11 @@
 import React from "react";
 
 function SelectionPage(props) {
-  console.log(props);
-  console.log(props.locationData[0].city);
   return (
     <div>
      <div>
       {/* <h1>Breweries in {(props[0].city, props[0].state)}</h1> */}
-        {/* <h1>{props.locationData.name}</h1> */}
+        <h1>{props.locationData.name}</h1>
     </div>
     </div>
   );
