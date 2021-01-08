6cbef8d79da892032a3b32cbd2f5c8e97ccb5c60
Danielle Koski <dkoski4@yahoo.com>
2021-01-08 08:23:36 -0800

    fixed footer to bottom and styling

--- src/pages/SelectionPage/SelectionPage.js
+++ src/pages/SelectionPage/SelectionPage.js
@@ -2,11 +2,13 @@ import React from "react";
 
 function SelectionPage(props) {
   return (
+    <main>
     <div>
      <div>
         {props.locationData.map((brewery, idx)=><h1>{brewery.name}</h1>)}
     </div>
     </div>
+    </main>
   );
 }
  
