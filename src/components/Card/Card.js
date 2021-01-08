b8708cca9eafaabf6b46d35d2d6c7993f1e808a2
Steven Hong <steven.hong6ec@gmail.com>
2021-01-08 12:20:59 -0500

    added card

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,7 +1,19 @@
 import React from "react";
 
-function Card() {
-  return <div></div>;
+function Card(props) {
+  const brewery = props.brewery;
+  const key = props.key;
+
+  return (
+    <div>
+      <h1>{brewery.name}</h1>
+      <p>{brewery.description}</p>
+      <p>{brewery.address}</p>
+      <a href={brewery.website} target="_blank">
+        {brewery.website}
+      </a>
+    </div>
+  );
 }
 
 export default Card;
