49d28e8e826db6d91c18e869916e2357dd439a33
jared Long <jlong4223@gmail.com>
2021-01-08 15:16:16 -0600

    fixes form page styles

--- src/pages/NewBreweryPage/NewBreweryPage.js
+++ src/pages/NewBreweryPage/NewBreweryPage.js
@@ -2,15 +2,13 @@ import { useState } from "react";
 import { handleAddData } from "../../services/RailsApi";
 
 const NewBreweryPage = (props) => {
-  const [formState, setFormState] = useState([
-    {
-      name: "",
-      description: "",
-      address: "",
-      website: "",
-      location_id: 0,
-    },
-  ]);
+  const [formState, setFormState] = useState({
+    name: "",
+    description: "",
+    address: "",
+    website: "",
+    // location_id: 0,
+  });
 
   function handleChange(e) {
     setFormState((prevState) => ({
@@ -21,6 +19,7 @@ const NewBreweryPage = (props) => {
 
   async function handleSubmit(e) {
     e.preventDefault();
+    console.log(formState);
     try {
       await handleAddData(formState);
     } catch (e) {
@@ -29,58 +28,60 @@ const NewBreweryPage = (props) => {
   }
 
   return (
-    <div>
+    <main>
       <div>
-        Hi There, this is the form for adding breweries to the clicked cities
-        list.
-        {props.locationData.map((brewery) => (
-          <h1>{brewery.name}</h1>
-        ))}
-      </div>
+        <div>
+          Hi There, this is the form for adding breweries to the clicked cities
+          list.
+          {props.locationData.map((brewery, idx) => (
+            <h1 key={idx}>{brewery.name}</h1>
+          ))}
+        </div>
 
-      <form onSubmit={handleSubmit}>
-        <fieldset>
-          <legend>Add a new Brewery</legend>
-          <input
-            type="text"
-            placeholder="name"
-            name="name"
-            value={formState.name}
-            onChange={handleChange}
-          ></input>
-          <input
-            type="text"
-            placeholder="description"
-            name="description"
-            value={formState.description}
-            onChange={handleChange}
-          ></input>
-          <input
-            type="text"
-            placeholder="address"
-            name="address"
-            value={formState.address}
-            onChange={handleChange}
-          ></input>
-          <input
-            type="text"
-            placeholder="website"
-            name="website"
-            value={formState.website}
-            onChange={handleChange}
-          ></input>
-          <input
+        <form onSubmit={handleSubmit}>
+          <fieldset>
+            <legend>Add a new Brewery</legend>
+            <input
+              type="text"
+              placeholder="name"
+              name="name"
+              value={formState.name}
+              onChange={handleChange}
+            ></input>
+            <input
+              type="text"
+              placeholder="description"
+              name="description"
+              value={formState.description}
+              onChange={handleChange}
+            ></input>
+            <input
+              type="text"
+              placeholder="address"
+              name="address"
+              value={formState.address}
+              onChange={handleChange}
+            ></input>
+            <input
+              type="text"
+              placeholder="website"
+              name="website"
+              value={formState.website}
+              onChange={handleChange}
+            ></input>
+            {/* <input
             type="text"
             placeholder="location id"
             value={formState.location_id}
             name="location_id"
             onChange={handleChange}
-          ></input>
-          {/* value/location=props.id something like that here */}
-          <button>Add Brewery</button>
-        </fieldset>
-      </form>
-    </div>
+          ></input> */}
+            {/* value/location=props.id something like that here */}
+            <button>Add Brewery</button>
+          </fieldset>
+        </form>
+      </div>
+    </main>
   );
 };
 
