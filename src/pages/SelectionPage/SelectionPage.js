cf50f94a5e114908cf246ef1d0b7b6480b6aecc2
Steven Hong <steven.hong6ec@gmail.com>
2021-01-08 15:11:34 -0500

    card css added

--- src/pages/SelectionPage/SelectionPage.js
+++ src/pages/SelectionPage/SelectionPage.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Card from "../../components/Card/Card";
+import "./SelectionPage.css";
 
 function SelectionPage(props) {
   return (
     <main>
-      <div>
-        <div>
+      <div className="Card__container">
+        <div className="Card__holder">
           {props.locationData.map((brewery, idx) => (
             <Card brewery={brewery} key={idx} />
           ))}
