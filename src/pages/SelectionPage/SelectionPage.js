ebba6402a00fc6449cf6e393f4daec072360c913
Steven Hong <steven.hong6ec@gmail.com>
2021-01-08 11:45:58 -0500

    added data to selection

--- src/pages/SelectionPage/SelectionPage.js
+++ src/pages/SelectionPage/SelectionPage.js
@@ -3,13 +3,22 @@ import React from "react";
 function SelectionPage(props) {
   return (
     <main>
-    <div>
-     <div>
-        {props.locationData.map((brewery, idx)=><h1>{brewery.name}</h1>)}
-    </div>
-    </div>
+      <div>
+        <div>
+          {props.locationData.map((brewery, idx) => (
+            <div>
+              <h1>{brewery.name}</h1>
+              <p>{brewery.description}</p>
+              <p>{brewery.address}</p>
+              <a href={brewery.website} target="_blank">
+                {brewery.website}
+              </a>
+            </div>
+          ))}
+        </div>
+      </div>
     </main>
   );
 }
- 
+
 export default SelectionPage;
