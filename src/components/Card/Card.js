fb806c75d05b09c75519e6e483b9ece3157440b3
jared Long <jlong4223@gmail.com>
2021-01-09 18:09:54 -0600

    sets up update function and edit form component

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,20 +1,42 @@
 import React from "react";
+import { useState } from "react";
 import "./Card.css";
+import EditForm from "../EditForm/EditForm";
+
+// TODO set up form state for the editform and pass that state as props to the editform and bring in the update function (similar to the newBreweryPage form)
 
 function Card(props) {
+  const [editFormVisible, setEditFormVisible] = useState(false);
+
+  function toggleForm() {
+    setEditFormVisible(!editFormVisible);
+  }
+
   const brewery = props.brewery;
   const key = props.key;
 
   return (
-    <div className="Card__base" key={key}>
-      <h1>{brewery.name}</h1>
-      <p>{brewery.description}</p>
-      <p>{brewery.address}</p>
-      <a href={brewery.website} target="_blank" rel="noreferrer noopener">
-        {brewery.website}
-      </a>
-      <button onClick={() => props.deleteBrewery(brewery)}>X</button>
-    </div>
+    <>
+      {editFormVisible ? (
+        <>
+          <div className="Card__base">
+            <EditForm brewery={brewery} />
+            <button onClick={toggleForm}>Cancel</button>
+          </div>
+        </>
+      ) : (
+        <div className="Card__base" key={key}>
+          <h1>{brewery.name}</h1>
+          <p>{brewery.description}</p>
+          <p>{brewery.address}</p>
+          <a href={brewery.website} target="_blank" rel="noreferrer noopener">
+            {brewery.website}
+          </a>
+          <button onClick={() => props.deleteBrewery(brewery)}>X</button>
+          <button onClick={toggleForm}>Edit</button>
+        </div>
+      )}
+    </>
   );
 }
 
---
+++ src/components/EditForm/EditForm.js
@@ -0,0 +1,12 @@
+const EditForm = (props) => {
+  return (
+    <div>
+      <h1>
+        <em>edit form will be here soon</em>
+      </h1>
+      <h1>{props.brewery.name}</h1>
+    </div>
+  );
+};
+
+export default EditForm;
